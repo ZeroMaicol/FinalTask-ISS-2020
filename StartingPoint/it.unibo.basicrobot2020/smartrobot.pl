0530c374e2a0627a8f6dcced33269c94bc16a091
maicol.forti <maicol.forti@studio.unibo.it>
2020-01-02 14:42:06 +0100

    Starting Point

---
+++ StartingPoint/it.unibo.basicrobot2020/smartrobot.pl
@@ -0,0 +1,8 @@
+%====================================================================================
+% smartrobot description   
+%====================================================================================
+context(ctxsmartrobot, "localhost",  "TCP", "8020").
+context(ctxbasicrobot, "192.168.1.6",  "TCP", "8018").
+ qactor( basicrobot, ctxbasicrobot, "external").
+  qactor( smartrobot, ctxsmartrobot, "it.unibo.smartrobot.Smartrobot").
+  qactor( perceiver, ctxsmartrobot, "it.unibo.perceiver.Perceiver").
