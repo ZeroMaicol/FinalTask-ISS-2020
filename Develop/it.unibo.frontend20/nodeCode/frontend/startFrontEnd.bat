b00e2ea86f0b6eaff4451f96ef72fa914490484f
maicol.forti <maicol.forti@studio.unibo.it>
2020-01-10 17:55:29 +0100

    Doc maicol sprint1

--- Develop/it.unibo.frontend20/nodeCode/frontend/startFrontEnd.bat
+++
@@ -1,7 +0,0 @@
-@rem #########################################
-@rem
-@rem  startFrontEnd.bat
-@rem
-@rem #########################################
-
-node frontendServer.js coap://localhost:5683/robot/pos
